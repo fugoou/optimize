11ec33e11d1792f94db9fcc872c41f1f969359b1
Nicko <89079801+fugoou@users.noreply.github.com>
2023-12-07 19:28:18 +0700

    Add files via upload

---
+++ menu/downscale.sh
@@ -0,0 +1,22 @@
+downscale () {
+echo ""
+echo ""
+animate_typing "The larger the number, the higher the resolution; the smaller the number, the lower the resolution." "41"
+echo ""
+animate_typing "recommendation 0.7-0.9" "42"
+echo ""
+echo ""
+echo -ne "${G}Enter Downscale Factor:${F} "
+read -s scale_factor
+
+current_resolution=$(wm size | awk '{print $3}')
+IFS='x' read -r width height <<< "$current_resolution"
+new_width=$(printf "%.0f" $(echo "$width * $scale_factor" | bc -l))
+new_height=$(printf "%.0f" $(echo "$height * $scale_factor" | bc -l))
+new_resolution="${new_width}x${new_height}"
+
+wm size $new_resolution
+
+echo ""
+echo "Resolution changed from $current_resolution to $new_resolution"
+}
