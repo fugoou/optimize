4188f1230184a67ea969b20ef500a047121f9c25
Nicko <89079801+fugoou@users.noreply.github.com>
2023-12-27 08:47:53 +0700

    .

--- menu/downscale.sh
+++ menu/downscale.sh
@@ -15,15 +15,18 @@ new_width=$(printf "%.0f" $(echo "$width * $scale_factor" | bc -l))
 new_height=$(printf "%.0f" $(echo "$height * $scale_factor" | bc -l))
 new_resolution="${new_width}x${new_height}"
 
-echo "this is preview of resolution $new_resolution, will reset again after 5 seconds"
+echo ""
+echo "${G}this is preview of resolution $new_resolution, will reset again after 5 seconds, Density size is automatically reset${F}"
 sleep 1
+wm density reset
 wm size $new_resolution
 sleep 5
 wm size reset
 
-echo -ne "${G}are you sure you want to change the resolution to $new_resolution? if yes, enter [ Y ], otherwise type anything except Y:${F} "
+echo ""
+echo -ne "${G}are you sure you want to change the resolution to $new_resolution? if yes, enter (y/n):${F} "
 read -s choice
-if [ "$choice" == "Y" ]; then
+if [ "$choice" == "y" ]; then
   wm size $new_resolution
   #echo "$new_resolution"
   echo ""
