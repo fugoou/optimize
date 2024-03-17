1abb8460259ee574bf6ed43de794af1d778afeb7
Nicko <89079801+fugoou@users.noreply.github.com>
2024-03-17 16:02:20 +0700

    Update custom_dns.sh

--- menu/custom_dns.sh
+++ menu/custom_dns.sh
@@ -3,7 +3,7 @@ echo ""
 echo ""
 printf "\e[101m\e[1;77mNOTE!\e[0m\n"
 echo ""
-echo "look at ${G}https://t.me/c/1932178615/42${F} for the best DNS recommendations"
+echo "look at ${G}https://github.com/fugoou/optimize/blob/main/DNS.md${F} for the best DNS recommendations"
 echo ""
 echo -ne "${G}Enter DNS hostname (E.g: dns.google):${F} "
 read -s ppdns
